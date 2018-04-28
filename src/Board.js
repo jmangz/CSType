50d2b7a74f9394881b865e79a028083a954c6f04
jmangz <echou1@binghamton.edu>
2018-04-28 00:12:10 -0400

    add board component

---
+++ src/Board.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Board = ({submit, click, addName, name, score, word, onGoing, reset}) => {
+  const gameOngoing = () => {
+    return <p id='word'>{word}</p>;
+  }
+
+  const gameOver = () => {
+    return (
+      <div>
+        <input className={submit ? 'nameInput': 'hide'} maxLength="12" type="text" onChange={ addName } autoFocus />
+        <input className={submit ? 'submitButton': 'hide'} type="button" value="Enter name" onClick={click}/>
+
+        <p id='endGameScore'>SCORE: {score}</p>
+        <button id='newGameBtn' onClick={reset}> NEW GAME </button>
+      </div>
+    )
+  }
+
+  let insideBox = null;
+  insideBox = onGoing ? gameOngoing() : gameOver();
+
+  return (
+    <div className='box'>
+      {insideBox}
+    </div>
+  );
+}
+
+export default Board;
